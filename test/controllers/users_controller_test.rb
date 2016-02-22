60209354605145cd6aa416a6de0bc6829c01d4c8
Kris Chery <kris.chery2@gmail.com>
2016-02-22 16:11:22 -0600

    Integrated test for user controller and test integration on site layouts

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
-#class UsersControllerTest < ActionController::TestCase
- # test "should get signup" do
- #   get :new
-  #  assert_response :success
-  #  assert_select "title", " signup | #{@base_title}"
-  #end
+class UsersControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
 end
